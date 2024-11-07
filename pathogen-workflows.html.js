22a38278cfca38120dc95919fad5cb51cb18a974
Thomas Sibley <tsibley@fredhutch.org>
2024-11-07 13:25:53 -0800

    Sort by repo and workflow names case-insensitively
    Otherwise outlier repos with uppercase, e.g. WNV, sort first
    unexpectedly.

--- pathogen-workflows.html.js
+++ pathogen-workflows.html.js
@@ -22,8 +22,8 @@ const runsByRepoAndWorkflow =
   groups(
     sort(
       workflowRuns,
-      d => d.repository_full_name,
-      d => d.workflow_name,
+      d => d.repository_full_name.toLowerCase(),
+      d => d.workflow_name.toLowerCase(),
       d => -d.created_at,
       d => -d.id),
     d => d.repository_full_name,
